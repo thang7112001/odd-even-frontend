35af548591b5415de90dbea38ba0c7d980240e65
Chien Thang <thangprovb@gmail.com>
2025-10-18 14:06:58 +0700

    fix text

--- src/component/Square.jsx
+++ src/component/Square.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Square({ handleClick, value }) {
   return (
-    <button className='w-full aspect-square bg-white text-black text-xl font-bold rounded-xl' onClick={handleClick}>
+    <button className='w-full aspect-square bg-white text-black text-3xl font-bold rounded-xl' onClick={handleClick}>
       {value}
     </button>
   );
